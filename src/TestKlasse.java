4ea68373841b3471ea2eaa9d7ff86734ebb8146e
Mangonssen <marc.obst@gmx.net>
2024-04-05 09:58:26 +0200

    TestKlasse gefüllt

--- src/TestKlasse.java
+++ src/TestKlasse.java
@@ -1,3 +1,20 @@
 public class TestKlasse {
 
+    private int testCount;
+    private String testString;
+    
+    public int getTestCount() {
+        return testCount;
+    }
+    public void setTestCount(int testCount) {
+        this.testCount = testCount;
+    }
+    public String getTestString() {
+        return testString;
+    }
+    public void setTestString(String testString) {
+        this.testString = testString;
+    }
+
+    
 }
